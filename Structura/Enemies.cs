c15c14469b9d7bb8cea6df46b9e02f9867b3f126
Vlad <wbcom9543@gmail.com>
2024-03-19 13:05:05 +0200

    firs commit

---
+++ Structura/Enemies.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Structura
+{
+    public abstract class Enemies
+    {
+        public int speed;
+        public PictureBox picture;
+        public abstract void move(Point point);
+        public abstract void Attack(Form1 f, Player p=null, Enemies b=null);
+    }
+    public class Bomb:Enemies
+    {
+        public Bomb(PictureBox p)
+        {
+            picture = p;
+            speed = 10;
+            attackStrategy = new BombStrategy();
+        }
+        AttackStrategy attackStrategy;
+
+        public override void Attack(Form1 f, Player p=null, Enemies b=null)
+        {
+            attackStrategy.Attack(p, b, f);
+        }
+        public override void move(Point point)
+        {
+           
+            if (this.picture.Right - (this.picture.Width / 2) <= point.X)
+            {
+                this.picture.Left += speed;
+            }
+            if (this.picture.Left + (this.picture.Width / 2) >= point.X)
+            {
+                this.picture.Left -= speed;
+            }
+
+            if (this.picture.Bottom - (this.picture.Height / 2) <= point.Y)
+            {
+                this.picture.Top += speed;
+            }
+            if (this.picture.Top + (this.picture.Height / 2) >= point.Y)
+            {
+                this.picture.Top -= speed;
+            }
+        }
+    }
+    public class Ghost : Enemies
+    {
+        public Ghost(PictureBox p)
+        {
+            picture = p;
+            speed = 5;
+            attackStrategy = new GhostStrategy();
+        }
+        AttackStrategy attackStrategy;
+
+        public override void Attack(Form1 f, Player p = null, Enemies b = null)
+        {
+            attackStrategy.Attack(p, b, f);
+        }
+        public override void move(Point point)
+        {
+
+            if (this.picture.Right - (this.picture.Width / 2) <= point.X)
+            {
+                this.picture.Left += speed;
+            }
+            if (this.picture.Left + (this.picture.Width / 2) >= point.X)
+            {
+                this.picture.Left -= speed;
+            }
+
+            if (this.picture.Bottom - (this.picture.Height / 2) <= point.Y)
+            {
+                this.picture.Top += speed;
+            }
+            if (this.picture.Top + (this.picture.Height / 2) >= point.Y)
+            {
+                this.picture.Top -= speed;
+            }
+        }
+    }
+    public class Striker : Enemies
+    {
+        public Striker(PictureBox p)
+        {
+            picture = p;
+            speed = 1;
+            attackStrategy = new StrikerStrategy();
+        }
+        AttackStrategy attackStrategy;
+
+        public override void Attack(Form1 f, Player p = null, Enemies b = null)
+        {
+            attackStrategy.Attack(p, b, f);
+        }
+        public override void move(Point point)
+        {
+
+            if (this.picture.Right - (this.picture.Width / 2) <= point.X)
+            {
+                this.picture.Left += speed;
+            }
+            if (this.picture.Left + (this.picture.Width / 2) >= point.X)
+            {
+                this.picture.Left -= speed;
+            }
+            if (this.picture.Bottom - (this.picture.Height / 2) <= point.Y)
+            {
+                this.picture.Top += speed;
+            }
+            if (this.picture.Top + (this.picture.Height / 2) >= point.Y)
+            {
+                this.picture.Top -= speed;
+            }
+        }
+    }
+    public class Player:Enemies
+    {
+        public Player(PictureBox p)
+        {
+            picture = p;
+            speed = 7;
+        }
+        public int health = 100;
+        int AttackRadius = 500;
+        public List<Enemies> enemies = new List<Enemies>();
+        public override void Attack(Form1 f, Player p=null, Enemies b=null) {
+            for (int i = 0; i < enemies.Count; i++)
+            {
+                enemies[i].Attack( f, this, enemies[i]);
+            }
+    }
+        public override void move(Point point)
+        {
+            this.picture.Left = point.X;
+            this.picture.Top = point.Y;
+            
+
+            for(int i=0 ; i < enemies.Count; i++)
+            {
+                if (new Rectangle(this.picture.Location.X + ((this.picture.Width - AttackRadius) / 2), this.picture.Location.Y + ((this.picture.Height - AttackRadius) / 2), AttackRadius, AttackRadius).Contains( enemies[i].picture.Location))
+                {
+                     enemies[i].move(new Point(point.X + (this.picture.Width / 2), point.Y + (this.picture.Height / 2)));
+                }
+            }
+        }
+    }
+}
---
+++ Structura/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Structura
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Player player;
+        public int X = 0;
+        public int Y = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            player = new Player(pictureBox2);
+            player.enemies.Add(new Bomb(pictureBox1));
+            player.enemies.Add(new Ghost(pictureBox3));
+            player.enemies.Add(new Striker(pictureBox4));
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            player.move(new Point(X, Y));
+            player.Attack(this);
+           progressBar1.Value= player.health;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = !timer1.Enabled;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            X = e.X - pictureBox1.Width / 2;
+            Y = e.Y - pictureBox1.Height / 2;
+        }
+    }
+}
